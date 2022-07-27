93854136b0485d67591b2f1500a77ad9fed2ab72
unknown <alexsandrapcampos03@gmail.com>
2022-07-27 12:21:32 -0300

    atualizaçao

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 require("dotenv").config()
 const express = require("express")
-const cors = require ("cors")
+const cors = require("cors")
 const mongoose = require("./database/mongooseConnect")
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger/swagger_output.json');
 const cursosRoutes =require("./Router/cursosRouter")
 const loginRoutes =require("./Router/loginRouter")
-const index =require ("./Router/index")
+const index =require("./Router/index")
 const app = express()
 
 app.use(express.json())
